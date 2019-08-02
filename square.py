817b112be290a0685d239bc260eb858c7f91c899
eric-bray <44974951+eric-bray@users.noreply.github.com>
2019-08-02 15:14:24 -0400

    Create square.py

---
+++ square.py
@@ -0,0 +1,23 @@
+# EdCoWNY STEAM (08/2019) - turtle demo (by Eric Bray, ebray@gow.org)
+# square.py
+
+import turtle
+
+leo = turtle.Turtle()
+canvas = turtle.Screen()
+
+leo.shape("turtle")
+leo.color("green")
+
+leo.forward(100)
+leo.left(90)
+
+leo.forward(100)
+leo.left(90)
+
+leo.forward(100)
+leo.left(90)
+
+
+
+canvas.exitonclick()
